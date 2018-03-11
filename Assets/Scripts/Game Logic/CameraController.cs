c83b3b69fb89f376ed58f683454836658bc14521
Joonas Kajava <jomppe@saviruukku.fi>
2018-03-11 18:07:18 +0200

    new animations

---
+++ Assets/Scripts/Game Logic/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float DampTime = 0.15f;
+    private Vector3 Velocity = Vector3.zero;
+
+    public Transform Target;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Target)
+        {
+            Vector3 Point = Camera.main.WorldToViewportPoint(Target.position);
+            Vector3 Delta = Target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, Point.z));
+            Vector3 Destination = transform.position + Delta;
+            transform.position = Vector3.SmoothDamp(transform.position, Destination, ref Velocity, DampTime);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,63 +1,90 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     private CharacterController c_controller;
     private SpriteRenderer s_renderer;
     private Animator animator;
     private Vector3 moveDirection = Vector3.zero;
+    private bool Jumping;
+    private bool Falling;
 
     public float Gravity = 9.8f;
     public float Speed;
     public float JumpForce = 8.0f;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         c_controller = GetComponent<CharacterController>();
         s_renderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         float h_axis = Input.GetAxis("Horizontal");
 
         if (h_axis < 0)
         {
             s_renderer.flipX = true;
-        }else if(h_axis > 0)
+        }
+        else if (h_axis > 0)
         {
             s_renderer.flipX = false;
         }
-        if(c_controller.isGrounded)
+        if (c_controller.isGrounded)
         {
-            if(h_axis != 0)
+            if(Jumping || Falling)
+            {
+                animator.Play("PlayerIdle");
+            }
+            Jumping = false;
+            Falling = false;
+            moveDirection = new Vector3(h_axis, 0, 0);
+            if (h_axis != 0)
             {
                 animator.SetBool("Walking", true);
-            }else
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= Speed;
+            }
+            else
             {
                 animator.SetBool("Walking", false);
             }
-            moveDirection = new Vector3(h_axis, 0, 0);
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= Speed;
+
+
 
             if (Input.GetButton("Jump"))
             {
                 moveDirection.y = JumpForce;
+                animator.Play("Jump");
+                Jumping = true;
             }
-        }else
+        }
+        else
         {
+            if(!Jumping && !Falling)
+            {
+                Falling = true;
+                animator.Play("Falling");
+            }
             Vector3 AirMove = new Vector3(h_axis, 0, 0);
-            if(h_axis != 0)
+            if (h_axis != 0)
             {
                 moveDirection.x = AirMove.x;
             }
             AirMove *= Speed;
             c_controller.Move(AirMove * Time.deltaTime);
+
         }
         moveDirection.y -= Gravity * Time.deltaTime;
 
         c_controller.Move(moveDirection * Time.deltaTime);
-	}
+
+        Debug.Log(c_controller.isGrounded);
+
+    }
 }
